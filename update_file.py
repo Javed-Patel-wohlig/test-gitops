1e8232986780e005ba783cbc904974bf5b2da55b
Javed-Patel-wohlig <javed.patel@wohlig.com>
2024-01-11 10:49:30 +0530

    remove the git commits

--- update_file.py
+++ update_file.py
@@ -16,8 +16,3 @@ count = int(count) + 50
 # Update file content
 with open(file_path, "w") as file:
     file.write(f"hello i am devops\nmy commit count is: {count}\nthank you")
-
-# Commit and push changes
-os.system("git add .")
-os.system("git commit -m 'Update commit count'")
-os.system(f"git push https://Javed-Patel-wohlig:${{ secrets.TOKEN100 }}@github.com/Javed-Patel-wohlig/test-gitops.git HEAD:main")
